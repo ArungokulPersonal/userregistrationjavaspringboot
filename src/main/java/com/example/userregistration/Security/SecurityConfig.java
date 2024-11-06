0bcb7828d92849618d349b717e42de1dce42a9c0
ArungokulPersonal <goksversatile@gmail.com>
2024-11-06 16:48:44 +0530

    Creating the http basic login auth.

--- src/main/java/com/example/userregistration/Security/SecurityConfig.java
+++ src/main/java/com/example/userregistration/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -48,10 +49,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
             .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(httpForm ->{
-                httpForm.loginPage("/req/login").permitAll();
-                httpForm.defaultSuccessUrl("/index");
-            })
+            // .formLogin(httpForm ->{
+            //     httpForm.loginPage("/req/login").permitAll();
+            //     httpForm.defaultSuccessUrl("/index");
+            // })
+            .httpBasic(Customizer.withDefaults())
     
             
             .authorizeHttpRequests(registry ->{
